dd36aca1e369cf4b1b4b3453d2cc5518889d6566
Shin Sie On <coolguysiun@gmail.com>
2023-02-22 21:45:18 +0900

    Initial Commit

---
+++ WakeUp/WakeUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  WakeUpApp.swift
+//  WakeUp
+//
+//  Created by 신시언 on 2023/02/22.
+//
+
+import SwiftUI
+
+@main
+struct WakeUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
